493650d055aebafa5926af1c786787eed53b47dc
Martijn Haex <martijn.haex@jidoka.be>
2023-12-06 06:55:52 +0100

    Include timings in PuzzleSolver output.

--- src/main/java/be/haex/puzzle/PuzzleSolver.java
+++ src/main/java/be/haex/puzzle/PuzzleSolver.java
@@ -7,8 +7,11 @@ import be.haex.puzzle.day.PuzzleDaySix;
 import be.haex.puzzle.day.PuzzleDayThree;
 import be.haex.puzzle.day.PuzzleDayTwo;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Map;
 import java.util.Optional;
+import java.util.function.Function;
 
 public class PuzzleSolver {
 
@@ -20,13 +23,28 @@ public class PuzzleSolver {
 		System.out.println("Advent of Code 2023 --- Day " + day);
 
 		var puzzle = loadPuzzle(day);
-		System.out.println("Your puzzle answer for Part One is " + puzzle.solvePartOne());
-		System.out.println("Your puzzle answer for Part Two is " + puzzle.solvePartTwo());
+
+		var solutionPartOne = solve(puzzle, Puzzle::solvePartOne);
+		System.out.printf("Your puzzle answer for Part One is %s (in %s)%n", solutionPartOne.value(), solutionPartOne.duration());
+
+		var solutionPartTwo = solve(puzzle, Puzzle::solvePartTwo);
+		System.out.printf("Your puzzle answer for Part Two is %s (in %s)%n", solutionPartTwo.value(), solutionPartTwo.duration());
 	}
 
 	private static Puzzle<?> loadPuzzle(Integer dayOfAdvent) {
 		return Optional.ofNullable(PUZZLES.get(dayOfAdvent))
 				.orElseThrow(() -> new IllegalArgumentException("Cannot load puzzle for day <" + dayOfAdvent + "> of advent 2023!"));
+	}
+
+	private static Solution<?> solve(Puzzle<?> puzzle, Function<Puzzle<?>, ?> solver) {
+		var start = Instant.now();
+		var solution = solver.apply(puzzle);
+		var stop = Instant.now();
+
+		return new Solution<>(solution, Duration.between(start, stop));
+	}
+
+	private record Solution<T>(T value, Duration duration) {
 	}
 
 	static {
